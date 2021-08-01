600da44afb4933fe0793ea0e84dae1e8043294ee
Carlos Ruiz <d34dplayer@protonmail.com>
2021-08-01 23:30:23 +0200

    Fix chapter number

--- api/models/chapter.py
+++ api/models/chapter.py
@@ -11,7 +11,7 @@ class Chapter(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String, nullable=False)
     volume = Column(Integer, nullable=True)
-    number = Column(Integer, nullable=False)
+    number = Column(String, nullable=False)
     length = Column(Integer, nullable=False)
     manga_id = Column(UUID(as_uuid=True), ForeignKey('manga.id', ondelete="CASCADE"), nullable=False)
     manga = relationship("Manga", back_populates="chapters")
