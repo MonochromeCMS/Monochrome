0dbaf7f6b13557c5a939d094be36e040c02f237f
Carlos Ruiz <d34dplayer@protonmail.com>
2021-08-15 22:41:10 +0200

    Dynamic opensearch.xml generation
    "Dynamic" since it's only generated once at build, but that's still something

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,24 +1,28 @@
 module.exports = {
   root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
-  ],
+  env: { node: true },
   parserOptions: {
     ecmaVersion: 2020,
   },
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "@typescript-eslint/no-explicit-any": "off",
   },
   overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx", "**/*.vue"],
+      extends: [
+        "plugin:vue/essential",
+        "eslint:recommended",
+        "@vue/typescript/recommended",
+        "@vue/prettier",
+        "@vue/prettier/@typescript-eslint",
+      ],
+      rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+      },
+    },
     {
       files: [
         "**/__tests__/*.{j,t}s?(x)",
--- web/vue.config.js
+++ web/vue.config.js
@@ -1,3 +1,24 @@
+const path = require("path");
+const XMLPlugin = require("xml-webpack-plugin");
+
+const XMLFiles = [
+  {
+    template: path.join(__dirname, "./src/opensearch.ejs"),
+    filename: "opensearch.xml",
+    data: {
+      domainName: process.env.VUE_APP_DOMAIN_NAME,
+      protocol: process.env.VUE_APP_PROTOCOL,
+    },
+  },
+];
+
 module.exports = {
   transpileDependencies: ["vuetify"],
+  configureWebpack: {
+    plugins: [
+      new XMLPlugin({
+        files: XMLFiles,
+      }),
+    ],
+  },
 };
