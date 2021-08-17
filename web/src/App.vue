e21eb32dc914fd57a3b4c5e7dfa2ecd11d9ff9de
Carlos Ruiz <d34dplayer@protonmail.com>
2021-08-17 21:32:13 +0200

    Dynamic metadata
    Generate metadata in the prerenderer for twitter, discord,...

--- web/src/App.vue
+++ web/src/App.vue
@@ -8,7 +8,7 @@
   </v-app>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from "vue";
 import NavBar from "./components/NavBar.vue";
 import ThemeToggler from "@/components/ThemeToggler.vue";
@@ -19,6 +19,63 @@ export default Vue.extend({
   components: {
     ThemeToggler,
     NavBar,
+  },
+  metaInfo() {
+    const protocol = process.env.VUE_APP_PROTOCOL || "http";
+    const domain = process.env.VUE_APP_DOMAIN_NAME || "localhost";
+    const title = process.env.VUE_APP_TITLE || "Monochrome";
+    const description = process.env.VUE_APP_DESCRIPTION || "A website for reading manga";
+    return {
+      title,
+      titleTemplate: `%s | ${title}`,
+      meta: [
+        {
+          property: "twitter:title",
+          content: title,
+        },
+        {
+          vmid: "title",
+          property: "og:title",
+          content: title,
+        },
+        {
+          property: "twitter:card",
+          content: "summary",
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          property: "og:image",
+          content: `${protocol}://${domain}/img/icons/android-chrome-512x512.png`,
+        },
+        {
+          property: "twitter:image",
+          content: `${protocol}://${domain}/img/icons/android-chrome-512x512.png`,
+        },
+        {
+          property: "og:url",
+          content: `${protocol}://${domain}`,
+        },
+        {
+          property: "og:description",
+          content: description,
+        },
+        {
+          property: "twitter:description",
+          content: description,
+        },
+        {
+          name: "description",
+          content: description,
+        },
+        {
+          property: "og:locale",
+          content: "en_US",
+        },
+      ],
+    };
   },
   computed: {
     isConnected: function () {
--- web/src/main.ts
+++ web/src/main.ts
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import VueMeta from "vue-meta";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
@@ -8,6 +9,8 @@ import vuetify from "./plugins/vuetify";
 import "./app.scss";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueMeta);
 
 new Vue({
   router,
