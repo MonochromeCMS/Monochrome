4699b91f60baaff002709a48dd2568544920ccb8
Carlos Ruiz <d34dplayer@protonmail.com>
2021-08-18 17:02:30 +0200

    Dynamic metadata
    Let's just use htmlWebpack isntead of vue-meta + prerender because it's broken as fuck

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,19 +1,34 @@
 const path = require("path");
 const XMLPlugin = require("xml-webpack-plugin");
 
+const protocol = process.env.VUE_APP_PROTOCOL || "http";
+const domainName = process.env.VUE_APP_DOMAIN_NAME || "localhost";
+
 const XMLFiles = [
   {
     template: path.join(__dirname, "./src/opensearch.ejs"),
     filename: "opensearch.xml",
     data: {
-      domainName: process.env.VUE_APP_DOMAIN_NAME,
-      protocol: process.env.VUE_APP_PROTOCOL,
+      domainName,
+      protocol,
     },
   },
 ];
 
+const metaArgs = {
+  title: process.env.VUE_APP_TITLE || "Monochrome",
+  url: `${protocol}://${domainName}/`,
+  description: process.env.VUE_APP_DESCRIPTION || "A website for reading manga",
+};
+
 module.exports = {
   transpileDependencies: ["vuetify"],
+  chainWebpack: config => {
+    config.plugin('html').tap((args) => {
+      args[0] = {...args[0], ...metaArgs};
+      return args;
+    });
+  },
   configureWebpack: {
     plugins: [
       new XMLPlugin({
