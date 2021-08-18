79cd4c13eba82b22de1c83743756f57c35758bd7
Carlos Ruiz <d34dplayer@protonmail.com>
2021-08-18 17:01:36 +0200

    Revert "Dynamic metadata"
    This reverts commit e21eb32dc914fd57a3b4c5e7dfa2ecd11d9ff9de.

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,4 @@
 import Vue from "vue";
-import VueMeta from "vue-meta";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
@@ -9,8 +8,6 @@ import vuetify from "./plugins/vuetify";
 import "./app.scss";
 
 Vue.config.productionTip = false;
-
-Vue.use(VueMeta);
 
 new Vue({
   router,
