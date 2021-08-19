9b341fac81d374f5708ed9273e45ffbf6d9cbdb8
Carlos Ruiz <d34dplayer@protonmail.com>
2021-08-19 14:28:30 +0200

    Add dynamic title to opensearch

--- web/vue.config.js
+++ web/vue.config.js
@@ -4,21 +4,22 @@ const XMLPlugin = require("xml-webpack-plugin");
 const protocol = process.env.VUE_APP_PROTOCOL || "http";
 const domainName = process.env.VUE_APP_DOMAIN_NAME || "localhost";
 const title = process.env.VUE_APP_TITLE || "Monochrome";
+const url = `${protocol}://${domainName}/`;
 
 const XMLFiles = [
   {
     template: path.join(__dirname, "./src/opensearch.ejs"),
     filename: "opensearch.xml",
     data: {
-      domainName,
-      protocol,
+      url,
+      title,
     },
   },
 ];
 
 const metaArgs = {
   title,
-  url: `${protocol}://${domainName}/`,
+  url,
   description: process.env.VUE_APP_DESCRIPTION || "A website for reading manga",
 };
 
